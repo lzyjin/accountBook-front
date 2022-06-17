cacb41498cb864ddf6e415f57b32b74e4ab07dea
newkayak12 <newkayak12@gmail.com>
2022-06-17 21:15:19 +0900

    Sorry this is my fault

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
-import Env from '@/common/constants/Env.json'
 module.exports = {
-    baseName: Env.PROJECT_NAME,
-    touchIconUrl:Env.TOUCH_ICON_PATH,
     devServer:{
         port:9001,
         proxy:{
